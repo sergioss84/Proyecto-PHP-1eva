cdad2461a0850d36d54b5094267dc09b9f7b5282
samuel <samuelsuarezlorenzo18@gmail.com>
2025-11-27 13:35:53 +0100

    cmabio3

--- src/tema.php
+++ src/tema.php
@@ -1,10 +1,8 @@
 <?php
-// Garantizamos que la sesión se inicia sin duplicados
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// Cambiar modo si se pulsa el enlace
 if (isset($_GET["modo"])) {
 
     if (!isset($_SESSION["modo"])) {
@@ -16,11 +14,10 @@ if (isset($_GET["modo"])) {
     }
 }
 
-// Valor por defecto
 $modo = $_SESSION["modo"] ?? "claro";
 
-// Asignación del CSS
 $css = ($modo == "oscuro") ? "css/oscuro.css" : "css/claro.css";
 ?>
+
 
 
